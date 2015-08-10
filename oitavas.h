acb03acca2a52f793eed2e4b56d0b3b522f7d906
Thiago <thiagoencomp@gmail.com>
2015-08-09 21:15:47 -0300

    Biblioteca com Oitavas

---
+++ oitavas.h
@@ -0,0 +1,34 @@
+//Primeira Oitava
+#define C_0 16.352
+#define Cm_0 16.352
+#define D_0 16.352
+#define Dm_0 16.352
+#define E_0 16.352
+#define F_0 16.352
+#define Fm_0 16.352
+#define G_0 16.352
+#define Gm_0 16.352
+#define _0 16.352
+#define C_0 16.352
+#define C_0 16.352
+//Segunda Oitava
+#define C_1 32.703
+//Terceira Oitava
+#define C_2 65.406
+//Quarta Oitava
+#define C_3 130.81
+//Quinta Oivata
+#define C_4 261.63
+//Sexta Oivata
+#define C_5 523.25
+//Setima Oivata
+#define C_6 1046.5
+//Oitava Oivata
+#define C_7 2093.0
+//Nona Oivata
+#define C_8 4186.0
+//Decima Oivata
+#define C_9 8372.0
+//Decima Primeira Oivata
+#define C_10 16744.0
+
